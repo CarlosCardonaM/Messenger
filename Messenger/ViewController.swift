5a0fc49af914f4f72acac1c0db045da7556d3c31
Carlos Cardona <carloscardona@MacBook-Pro-de-Carlos-Cardona.local>
2020-06-08 18:48:33 -0500

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Carlos Cardona on 08/06/20.
+//  Copyright © 2020 D O G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
