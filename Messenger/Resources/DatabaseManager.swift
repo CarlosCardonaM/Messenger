ad7736b96ab8253029dc344ba3aff7afa0d78311
Carlos Cardona <carloscardona@MacBook-Pro-de-Carlos-Cardona.local>
2020-06-13 17:04:51 -0500

    Handle auth session, add database

---
+++ Messenger/Resources/DatabaseManager.swift
@@ -0,0 +1,50 @@
+//
+//  DatabaseManager.swift
+//  Messenger
+//
+//  Created by Carlos Cardona on 13/06/20.
+//  Copyright © 2020 D O G. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+final class DatabaseManager {
+    
+    static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+    
+}
+
+// MARK: - Account Management
+extension DatabaseManager {
+    
+    public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
+        
+        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+            guard snapshot.value as? String  != nil else {
+                completion(false)
+                return
+            }
+            
+            completion(true)
+        })
+    }
+    
+    /// Inserts new user to database
+    public func insertUser(with user: ChatAppUser) {
+        database.child(user.emailAddress).setValue([
+            "firs_name": user.firstName,
+            "last_name": user.lastName
+        ])
+    }
+}
+
+struct ChatAppUser {
+    
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+//    let profilePictureURL: String
+}
